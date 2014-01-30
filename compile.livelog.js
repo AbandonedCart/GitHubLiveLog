1bc4c401db2a3990a11242ee4004048fc9e8ec42
TwistedUmbrella <twistedumbrella@gmail.com>
2014-01-30 13:01:03 -0500

    Add licensing information to javascript for public sourcing

--- compile.livelog.js
+++ compile.livelog.js
@@ -1,3 +1,58 @@
+/* ====================================================================
+ * Copyright (c) 2012-2013 Lounge Katt Entertainment.  All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ *
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ *
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in
+ *    the documentation and/or other materials provided with the
+ *    distribution.
+ *
+ * 3. All advertising materials mentioning features or use of this
+ *    software must display the following acknowledgment:
+ *    "This product includes software developed by Lounge Katt" unless
+ *    otherwise displayed by public repository entries.
+ *
+ * 4. The names "Lounge Katt", "TwistedUmbrella", and "LiveLog"
+ *    must not be used to endorse or promote products derived from this
+ *    software without prior written permission. For written permission,
+ *    please contact admin@loungekatt.com.
+ *
+ * 5. Products derived from this software may not be called "LiveLog"
+ *    nor may "LiveLog" appear in their names without prior written
+ *    permission of Lounge Katt Entertainment.
+ *
+ * 6. Redistributions of any form whatsoever must retain the following
+ *    acknowledgment:
+ *    "This product includes software developed by Lounge Katt" unless
+ *    otherwise displayed by tagged repository entries.
+ *
+ * THIS SOFTWARE IS PROVIDED BY Lounge Katt ``AS IS'' AND ANY
+ * EXPRESSED OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
+ * PURPOSE ARE DISCLAIMED.  IN NO EVENT SHALL THE OpenSSL PROJECT OR
+ * ITS CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+ * SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT
+ * NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+ * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT,
+ * STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED
+ * OF THE POSSIBILITY OF SUCH DAMAGE.
+ * ====================================================================
+ *
+ * The license and distribution terms for any publicly available version or
+ * derivative of this code cannot be changed.  i.e. this code cannot simply be
+ * copied and put under another distribution license
+ * [including the GNU Public License.] Content not subject to these terms is
+ * subject to to the terms and conditions of the Apache License, Version 2.0.
+ */
+
 var githubLive = function githubLive(account, developer, anchor, product, builds) {
         $.ajax({
             url: 'https://api.github.com/users/' + account + '/events?callback=jsonp',
